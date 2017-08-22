26df85fce4203c6a859e1109deaf719faebb1418
Weston Catron <weston@heatworks.tech>
2017-08-22 15:41:46 -0400

    Setup use prompt package.

--- start.js
+++ start.js
@@ -1,10 +1,5 @@
 var randomstring = require("randomstring");
 
-var organization = "heatworks"
-var deviceName = "mosfet8/unknown"
-var accessToken = "defaultAccessToken"
-
-
 console.log(`Using credential file: ${process.env.NODE_RED_CRED_FILE}`);
 console.log(`Using flow file: ${process.env.NODE_RED_FLOW_FILE}`);
 console.log(`Using settings file: ${process.env.NODE_RED_SETTINGS_FILE}`);
@@ -20,14 +15,52 @@ if (!("NODE_RED_SETTINGS_FILE" in process.env)) {
     throw new Error("Settings file (NODE_RED_SETTINGS_FILE) must be set in the environment variables.");
 }
 
-var setNodeREDSecurityKey = require("./basics.js").setNodeREDSecurityKey;
-var encryptNodeRED = require("./basics.js").encryptNodeRED;
-var updateFlowFile = require("./basics.js").updateFlowFile;
+var prompt = require('prompt');
 
-var randomUserKey = randomstring.generate();
+prompt.message = "hls";
+prompt.start();
 
-setNodeREDSecurityKey(randomUserKey);
+var schema = {
+    properties: {
+      organization: {
+        pattern: /^[a-zA-Z\-0-9]+$/,
+        message: 'Name must be only letters, numbers, or dashes',
+        required: true,
+        description: 'Enter the name of your organization as used on HLS',
+        default: 'hls'
+      },
+      deviceName: {
+        pattern: /^[a-zA-Z\-0-9\/]+$/,
+        message: 'Name must be only letters, numbers, dashes, or slashes',
+        description: 'Enter a name for the device (i.e. "mosfet8/B", "mosfet8/Station1")', 
+        default: 'mosfet8/unknown',       
+        required: true
+      },
+      accessToken: {
+        description: 'Enter Access Token for the device',                
+        hidden: true
+      }
+    }
+  };
 
-encryptNodeRED(randomUserKey, { 'd88144d2.de1c88': { user: 'HLS:AccessToken', password: accessToken } } )
 
-updateFlowFile(organization, deviceName)
+prompt.get(schema, function (err, result) {
+    
+    var organization = result.organization// "heatworks"
+    var deviceName = result.deviceName // "mosfet8/unknown"
+    var accessToken = result.accessToken // "defaultAccessToken"
+
+    var setNodeREDSecurityKey = require("./basics.js").setNodeREDSecurityKey;
+    var encryptNodeRED = require("./basics.js").encryptNodeRED;
+    var updateFlowFile = require("./basics.js").updateFlowFile;
+    
+    var randomUserKey = randomstring.generate();
+    
+    setNodeREDSecurityKey(randomUserKey);
+    
+    encryptNodeRED(randomUserKey, { 'd88144d2.de1c88': { user: 'HLS:AccessToken', password: accessToken } } )
+    
+    updateFlowFile(organization, deviceName)
+});
+
+
